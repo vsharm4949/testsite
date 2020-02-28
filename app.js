0528eda7a762a4709238fd90b98d05f09f6871e7
vsharm <vsharm@umich.edu>
2020-02-28 16:29:02 -0500

    sqlite3 working'

--- app.js
+++ app.js
@@ -11,6 +11,25 @@ var profileRouter = require('./routes/profile')
 
 var app = express();
 
+// SQLite3 test
+
+// Open database in memory
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./db/test.db', (err) => {
+  if(err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQLite database');
+});
+
+// Close the database connection
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.')
+});
+
 // view engine setup
 app.engine('pug', require('pug').__express)
 app.set('views', path.join(__dirname, 'views'));
@@ -41,6 +60,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+const server = app.listen(3000, () => {
+  console.log(`Express running → PORT ${server.address().port}`);
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,53 @@
 var express = require('express');
 var router = express.Router();
-const players = require('./players.json');
+//const players = require('./players.json');
+// Open database in memory
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./db/test.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQLite database');
+});
+
+
 
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Homepage', players: players.profiles });
+  let players = [];
+  db.all(`SELECT * FROM players`, [], (err, rows) => {
+  
+
+    if (err) {
+      res.status(500).json({ "status_code": 500, "status_message": "internal server error" });
+    }
+    else {
+      rows.forEach(row => {
+        // console.log(row.first);
+        var player = {
+          'firstname': row.first,
+          'lastname': row.last,
+          'imgSrc': row.imgSrc,
+          'id': row.id
+        }
+        players.push(player);
+      });
+      console.log(players);
+      console.log(players[0]);
+    }
+    res.render('index', { title: 'Homepage', players: players });
+  });
+
+  
   // res.send('Hello, world!')
 });
 
+// Close the database connection
+// db.close((err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log('Close the database connection.')
+// });
 module.exports = router;
